85eaba63e008f98edd725844561d4c631fd50caf
GopalSaraf <sarafgopal37@gmail.com>
2024-04-14 17:42:51 +0530

    Fixed initial broken things

--- backend-2-app/controller/user.controller.js
+++ backend-2-app/controller/user.controller.js
@@ -1,54 +1,53 @@
-const User = require('../models/user.models.js');
-const UserService = require('../services/user.services.js');
-const bcrypt = require('bcrypt');
+const User = require("../models/user.models.js");
+const UserService = require("../services/user.services.js");
+const bcrypt = require("bcrypt");
 
-exports.register = async(req, res, next) => {
-    try {
-        const {username, email, password} = req.body;
+exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
 
-        const success = await UserService.registerUser(username, email, password);
+    const success = await UserService.registerUser(username, email, password);
 
-        res.json({status:true, success: "User registered successfully"});
-        return ;
-    } catch (error) {
-        throw error
-    }
+    res.json({ status: true, success: "User registered successfully" });
+    return;
+  } catch (error) {
+    throw error;
+  }
 };
 
-exports.login = async(req, res, next) => {
-    try {
-        const {email, password} = req.body;
-        
-        let user = await UserService.loginUser(email);
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-        if(!user)
-        {
-            throw new Error("User doesnt exist");
-        }
-        
+    let user = await UserService.loginUser(email);
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        
-        // const isMatch = await user.comparePassword(password);
-        // const isMatch = await comparePasswords(password);
-        if(isMatch == false)
-        {
-            throw new Error('Password Invalid!');
-        }
-
-
-        //store data in jwt
-        let tokenData = {_id: User._id, email: User.email};
-
-        const token = await UserService.generateToken(tokenData, "secretkey", "1d");
-
-
-        res.status(200).json({ status: true, success: "sendData", token: token });
-
-    } catch (error) {
-        console.log(error, 'err---->');
-        throw error
+    if (!user) {
+      throw new Error("User doesnt exist");
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // const isMatch = await user.comparePassword(password);
+    // const isMatch = await comparePasswords(password);
+    if (isMatch == false) {
+      throw new Error("Password Invalid!");
+    }
+
+    //store data in jwt
+    let tokenData = { _id: User._id, email: User.email };
+
+    const token = await UserService.generateToken(tokenData, "secretkey", "1d");
+
+    res.status(200).json({
+      status: true,
+      success: "sendData",
+      token: token,
+      user: { email: user.email, username: user.username, id: user._id },
+    });
+  } catch (error) {
+    console.log(error, "err---->");
+    throw error;
+  }
 };
 
 // module.exports = register;
